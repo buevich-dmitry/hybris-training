8e0db674bfe6b62454295bb9b09cce13f3fd097e
Buevich Dmitry <buevichd@gmail.com>
2018-05-17 13:25:40 +0300

    Initial commit

---
+++ esfulfilmentprocess/gensrc/com/expertsoft/merchandise/fulfilmentprocess/constants/GeneratedEsFulfilmentProcessConstants.java
@@ -0,0 +1,44 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 16.05.2018 12:22:16                         ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2016 SAP SE
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of SAP
+ * Hybris ("Confidential Information"). You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the
+ * terms of the license agreement you entered into with SAP Hybris.
+ *  
+ */
+package com.expertsoft.merchandise.fulfilmentprocess.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedEsFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "esfulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedEsFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
